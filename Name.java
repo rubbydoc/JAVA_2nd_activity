3a5928f626845368d3c8df33c789dd7576dfe284
tinytinnn <21103823@usc.edu.ph>
2022-09-08 10:01:35 +0800

    Martonia writes a system print for her name and pushes to the repository

--- Name.java
+++ Name.java
@@ -2,6 +2,6 @@ public class Name {
     public static void main(String[] args) {
         System.out.println("Rubby Jane Doclara");
          System.out.println("Bebie Grace Balbuena");
-        System.out.println("Christine Marotnia");
+        System.out.println("Christine Martonia");
     }
 }
