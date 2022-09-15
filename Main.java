4fd3146bebb2efa074f7153f37cef972e3775dbf
Rubby Jane Doclara <rdoclara@gmail.com>
2022-09-15 20:52:58 +0800

    added the Main class file

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+
+    public static void main(String[] args){
+        Animal a = new Animal();
+        Cat c = new Cat();
+        Parrot p = new Parrot();
+        Goat g = new Goat();
+
+
+    }
+
+}
