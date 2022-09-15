a1d0ad7cb54eb6f5adeda70926a6e3d752e255cc
Rubby Jane Doclara <rdoclara@gmail.com>
2022-09-15 20:51:25 +0800

    added the Parrot object

---
+++ Parrot.java
@@ -0,0 +1,10 @@
+public class Parrot extends Animal{
+
+    int numOfWings;
+
+    public void walk(){
+        System.out.println("Walking while alternating the legs rather than pushing.");
+}
+
+
+}
