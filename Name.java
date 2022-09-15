7e3058c456a2aefd32b2ce6ee875838c37827e4c
Rubby Jane Doclara <rdoclara@gmail.com>
2022-09-15 21:26:00 +0800

    deleted Name.java

--- Name.java
+++
@@ -1,7 +0,0 @@
-public class Name {
-    public static void main(String[] args) {
-        System.out.println("Rubby Jane Doclara");
-         System.out.println("Bebie Grace Balbuena");
-        System.out.println("Christine Martonia");
-    }
-}
