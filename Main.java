a54490c74b957f562111e4f553aef59c45765b3d
Rubby Jane Doclara <rdoclara@gmail.com>
2022-09-16 16:05:12 +0800

    added the display function

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+        }
+
+public void display(){
+    Scanner s = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Choose operator: ");
+    System.out.println("A : Addition ");
+    System.out.println("B : Subtraction ");
+    System.out.println("C : Division ");
+    System.out.println("D : Multiplication ");
+
+
+
+    String operator = s.nextLine();  // Read user input
+    System.out.println("Enter first number: ");
+    String firstNum = s.nextLine();  // Read user input
+    System.out.println("Enter second number: ");
+    String secondNum = s.nextLine();  // Read user input
+    
+
+    if (operator.equals("a") || operator.equals("A")){
+        
+        System.out.println("Sum is: " + add.add(Double.parseDouble(firstNum), Double.parseDouble(secondNum)) );
+    }
+    else if (operator.equals("b") || operator.equals("B")) {
+        System.out.println("Difference is: " + minus.subtract(Double.parseDouble(firstNum), Double.parseDouble(secondNum)) );
+       
+    }else if (operator.equals("c") || operator.equals("C")) {
+        System.out.println("Quotient is: " + divide.divide(Double.parseDouble(firstNum), Double.parseDouble(secondNum)) );
+       
+    }else if (operator.equals("d") || operator.equals("D")) {
+        System.out.println("Product is: " + multiply.multiply(Double.parseDouble(firstNum), Double.parseDouble(secondNum)) );
+    }
+}
+}
+
+
+
+       
+
+
+      
